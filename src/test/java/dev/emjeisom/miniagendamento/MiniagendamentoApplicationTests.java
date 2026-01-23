bad629362cf5ce83678f8c916e0a70f41667271a
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-01-23 15:11:32 -0300

    Project Created

---
+++ src/test/java/dev/emjeisom/miniagendamento/MiniagendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emjeisom.miniagendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniagendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
